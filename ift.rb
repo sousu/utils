5253cfff2624ee22bc2b11f290751e7d5e4925c5
sousu <rawkranz@gmail.com>
2020-04-15 12:28:20 +0900

    mod_get_urls

--- ift.rb
+++ ift.rb
@@ -16,7 +16,7 @@ d = Selenium::WebDriver.for(:firefox)
 wait = Selenium::WebDriver::Wait.new(:timeout=>20)
 
 begin
-  puts "--- init ---"
+  puts "init"
   d.get('https://ifttt.com/login?wp_=1')
   u = wait.until{ d.find_element(:name,'user[username]') }
   u.send_keys(cnf['u'])
@@ -24,21 +24,19 @@ begin
   p.send_keys(cnf['p'])
   p.send_keys(:enter)
   sleep 5
-  x = '//*[@id="config-editor"]/div[3]/div/div[3]/div[2]/a'
 
-  puts "--- share ---"
-  d.get('https://ifttt.com/applets/88935713d/edit')
-  a = wait.until{ d.find_element(:xpath,x) }
-  a.click
-  sleep 5
-  puts "--- tabe ---"
-  d.get('https://ifttt.com/applets/102549512d/edit')
-  a = wait.until{ d.find_element(:xpath,x) }
-  a.click
-  sleep 5
+  x = '//*[@id="config-editor"]/div[3]/div/div[3]/div[2]/a'
+  cnf['l'].each do |url|
+    puts "update #{url}"
+    d.get(url)
+    a = wait.until{ d.find_element(:xpath,x) }
+    a.click
+    sleep 5
+  end
 
   #d.save_screenshot('./ss.png')
 ensure
+  puts "end"
   d.quit
 end
 
