cbe5d153fc5ba4f121dc4adacc53d9ea8177c0be
sousu <rawkranz@gmail.com>
2022-12-28 17:28:57 +0900

    ruby_update

--- anki.rb
+++ anki.rb
@@ -1,4 +1,4 @@
-#!/usr/local/bin/ruby
+#!/usr/bin/env ruby
 # -*- encoding: UTF-8 -*-
 
 require 'mechanize'
@@ -7,7 +7,8 @@ require 'nokogiri'
 class Anki
   LOGIN="https://ankiweb.net/account/login"
 
-  def initialize(config)
+  def initialize(config,cnt)
+    @c = cnt
     @a = Mechanize.new{|a| a.ssl_version="SSLv23"}
     @a.user_agent_alias = 'Windows Mozilla'
     page = @a.get(LOGIN)
@@ -15,18 +16,20 @@ class Anki
     form.field_with(:name => "username").value = config["u"]
     form.field_with(:name => "password").value = config["p"]
     page = @a.submit(form)
+    sleep 1
     @n = page.at('div.deckDueNumber').inner_text.gsub(/\r\n|\r|\n|\s|\t/,"")
   end
 
   def num
-    n = (7*(100-@n.to_i)/100).to_i
+    b = @c.to_i
+    n = (10*(b-@n.to_i)/b).to_i
     n >= 0 ? n : 0
   end
 end
 
 if $0 == __FILE__
   require 'yaml'
-  a = Anki.new(YAML.load_file(ARGV[0]))
+  a = Anki.new(YAML.load_file(ARGV[0]),ARGV[1])
 
   puts a.num
 end
